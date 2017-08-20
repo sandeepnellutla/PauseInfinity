a5decf5909a29e97049afb32fcf44df3b248edcd
sandeepnellutla <sandeep.nellutla@gmail.com>
2017-08-19 22:42:32 -0500

    dataservice

---
+++ InfinityDataService/InfinityDataService/DataFactory.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfinityDataService
+{
+    public class DataFactory:NancyModule
+    {
+        public DataFactory()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
